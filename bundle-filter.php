dcc0a7e28697b455fbbdb0d720a303b7b6006ab1
sabatohura <sabato@hura.one>
2022-10-05 15:26:28 +0200

    changed to php

--- bundle-filter.php
+++ bundle-filter.php
@@ -13,6 +13,12 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+        $title = [
+            
+        ];
+    
+    ?>
     <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
         <li class="nav-item" role="presentation">
           <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
